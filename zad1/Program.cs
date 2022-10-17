5dd8dc1a333c63872b5815dbc06d789388617055
Vyacheslav <elik@example.com>
2022-10-17 18:53:29 +0700

    add task#25

---
+++ zad1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*ЗАДАЧА № 25:Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16*/
+
+/*void GetSumm()
+{
+    Console.WriteLine("Введите число А: ");
+    int A = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число B: ");
+    int B = int.Parse(Console.ReadLine());
+    
+    double step = Math.Pow (A,B);
+    
+    Console.WriteLine($"число {А} в натуральной степени {B} =  {step}");
+}
+GetSumm();*/
+
+void GetSumm()
+{
+    Console.WriteLine("Введите число А: ");
+    int A = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число B: ");
+    int B = int.Parse(Console.ReadLine());
+    int step = A;
+    for (int i = 1; i < B; i++)
+    {
+        step = step*A;
+    }
+    
+    Console.WriteLine($"число А в натуральной степени B =  {step}");
+}
+GetSumm();
+
