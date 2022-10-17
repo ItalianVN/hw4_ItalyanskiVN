a2b179db67a9d13bc9e99bfd758b6c9b3d5faf29
Vyacheslav <elik@example.com>
2022-10-17 18:54:02 +0700

    add task#27

---
+++ zad2/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача №27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.*/
+
+void GetSumm()
+{
+    Console.WriteLine("Введите число N: ");
+    int num = int.Parse(Console.ReadLine());
+    int summ = 0;
+    int ostatok = 0;
+    while (num > 0)
+    {
+        ostatok = num%10;
+        summ = summ+ostatok;
+        num = num/10;
+         
+    }
+    Console.WriteLine($"Сумма цифр в числе {summ}");
+}
+GetSumm();
+
